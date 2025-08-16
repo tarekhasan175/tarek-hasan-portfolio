27b2506302322a6ff20493c64ccbb8f0ec6f1455
Tarek H <tarekhn175@gmail.com>
2025-08-16 16:09:18 +0600

    skils imgaes add

--- frontend/components/skills.js
+++ frontend/components/skills.js
@@ -1,190 +1,128 @@
 export default function Skill() {
-    return (
-        <section id="partners" className="section-partner style-2 section spacing-5 ">
-            <div className="row">
-                <div className="col-lg-5">
-                    <div className="heading-section ">
-                        <div
-                            className="tag-heading text-uppercase text-label font-3 letter-spacing-1 mb_32">
-                            Partner</div>
-                        <h3
-                            className="text_white fw-5 split-text effect-blur-fade">
-                            Trusted By 100+ Brands
-                            Worldwide</h3>
-                    </div>
-                </div>
-                <div className="col-lg-7">
-                    <div className="swiper tf-sw-partner wrap-partner position-2"
-                        data-preview="8" data-tablet="8"
-                        data-mobile-sm="6" data-mobile="4"
-                        data-space="15" data-space-md="30"
-                        data-space-lg="30">
-                        <div className="swiper-wrapper">
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-1 sz-100 scrolling-effect effectZoomIn"
-                                data-delay="0.3"><img
-                                    src="assets/images/logo/partner-1.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-2 sz-80 scrolling-effect effectZoomIn"><img
-                                    src="assets/images/logo/partner-3.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-3 sz-80 scrolling-effect effectZoomIn"><img
-                                    src="assets/images/logo/partner-2.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-4 sz-60 scrolling-effect effectZoomIn"><img
-                                    src="assets/images/logo/partner-5.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-5 sz-60 scrolling-effect effectZoomIn"><img
-                                    src="assets/images/logo/partner-8.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-6 sz-100 scrolling-effect effectZoomIn"><img
-                                    src="assets/images/logo/partner-6.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-7 sz-200 scrolling-effect effectZoomIn"
-                                data-delay="0.4"><img
-                                    src="assets/images/logo/partner-9.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-8 sz-160 scrolling-effect effectZoomIn"
-                                data-delay="0.5"><img
-                                    src="assets/images/logo/partner-4.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-9 sz-120 scrolling-effect effectZoomIn"
-                                data-delay="0.6"><img
-                                    src="assets/images/logo/partner-7.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-10 sz-120 scrolling-effect effectZoomIn"
-                                data-delay="0.7"><img
-                                    src="assets/images/logo/partner-7.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-11 sz-100 scrolling-effect effectZoomIn"><img
-                                    src="assets/images/logo/partner-6.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                            <div className="swiper-slide"><a
-                                href="#"
-                                className="partner-item item-12 sz-80 scrolling-effect effectZoomIn"><img
-                                    src="assets/images/logo/partner-3.png"
-                                    alt="partner"
-                                    loading="lazy" />
-                                <div
-                                    className="item-shape">
-                                    <img src="assets/images/item/small-comet.webp"
-                                        alt="item"
-                                        loading="lazy" />
-                                </div>
-                            </a></div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section
+      id="partners"
+      className="section-partner style-2 section spacing-5 "
+    >
+      <div className="row">
+        <div className="col-lg-5">
+          <div className="heading-section ">
+            <div className="tag-heading text-uppercase text-label font-3 letter-spacing-1 mb_32">
+              Skills
             </div>
-        </section>
-    );
+            <h3 className="text_white fw-5 split-text effect-blur-fade">
+              My Skills
+            </h3>
+          </div>
+          <div className="">
+            <ul className="mx-4 mt-4">
+              <li className="p-2 text_white">PHP</li>
+              <li className="p-2 text_white">Laravel</li>
+              <li className="p-2 text_white">Node JS</li>
+              <li className="p-2 text_white">Vue JS</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-lg-7">
+          <div
+            className="swiper tf-sw-partner wrap-partner position-2"
+            data-preview="8"
+            data-tablet="8"
+            data-mobile-sm="6"
+            data-mobile="4"
+            data-space="15"
+            data-space-md="30"
+            data-space-lg="30"
+          >
+            <div className="swiper-wrapper">
+              <div className="swiper-slide">
+                <a
+                  href="#"
+                  className="partner-item item-1 sz-100 scrolling-effect effectZoomIn"
+                  data-delay="0.3"
+                >
+                  <img
+                    src="assets/images/logo/bootstrap_logo.png"
+                    alt="partner"
+                    loading="lazy"
+                    width={100}
+                  />
+                  <div className="item-shape">
+                    <img
+                      src="assets/images/item/small-comet.webp"
+                      alt="item"
+                      loading="lazy"
+                    />
+                  </div>
+                </a>
+              </div>
+              <div className="swiper-slide">
+                <a
+                  href="#"
+                  className="partner-item item-1 sz-100 scrolling-effect effectZoomIn"
+                  data-delay="0.3"
+                >
+                  <img
+                    src="assets/images/logo/php_logo.png"
+                    alt="partner"
+                    loading="lazy"
+                    width={100}
+                  />
+                  <div className="item-shape">
+                    <img
+                      src="assets/images/item/small-comet.webp"
+                      alt="item"
+                      loading="lazy"
+                    />
+                  </div>
+                </a>
+              </div>
+              <div className="swiper-slide">
+                <a
+                  href="#"
+                  className="partner-item item-1 sz-100 scrolling-effect effectZoomIn"
+                  data-delay="0.3"
+                >
+                  <img
+                    src="assets/images/logo/postman_logo.webp"
+                    alt="partner"
+                    loading="lazy"
+                    width={100}
+                  />
+                  <div className="item-shape">
+                    <img
+                      src="assets/images/item/small-comet.webp"
+                      alt="item"
+                      loading="lazy"
+                    />
+                  </div>
+                </a>
+              </div>
+              <div className="swiper-slide">
+                <a
+                  href="#"
+                  className="partner-item item-1 sz-100 scrolling-effect effectZoomIn"
+                  data-delay="0.3"
+                >
+                  <img
+                    src="assets/images/logo/nextjs_logo.svg"
+                    alt="partner"
+                    loading="lazy"
+                    width={100}
+                  />
+                  <div className="item-shape">
+                    <img
+                      src="assets/images/item/small-comet.webp"
+                      alt="item"
+                      loading="lazy"
+                    />
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
